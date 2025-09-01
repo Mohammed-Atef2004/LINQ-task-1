59be9f47200df8834d68de0c5aad1c659ad76ba7
Mohammed Atef <mohammedatef.8224@gmail.com>
2025-09-01 16:15:20 +0300

    Add project files.

---
+++ LINQ task 1/LINQ methods.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_task_1
+{
+    public class LINQ_methods
+    {
+        public static IEnumerable<T> Where<T>(IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            List<T> result = new List<T>();
+            foreach (var item in source)
+            {
+                //print doesn't represent the actual functionality of where in LINQ so I used list to store the result
+                if (predicate(item))
+                {
+                    result.Add(item);
+                }
+            }
+            return result;
+        }
+
+
+        public static IEnumerable<T> WhereIndex<T>(IEnumerable<T> source, Func<T, int, bool> predicate)
+        {
+            List<T> result = new List<T>();
+            int index = 0;
+            foreach (var item in source)
+            {
+                if (predicate(item, index))
+                {
+                    result.Add(item);
+                }
+                index++;
+            }
+            return result;
+        }
+        public static IEnumerable<TResult> Select<T, TResult>(IEnumerable<T> source, Func<T, TResult> selector)
+        {
+            List<TResult> result = new List<TResult>();
+            foreach (var item in source)
+            {
+                result.Add(selector(item));
+            }
+            return result;
+        }
+        public static IEnumerable<TResult> SelectIndex<T, TResult>(IEnumerable<T> source, Func<T, int, TResult> selector)
+        {
+            List<TResult> result = new List<TResult>();
+            int index = 0;
+            foreach (var item in source)
+            {
+                result.Add(selector(item, index));
+                index++;
+            }
+            return result;
+        }
+        public static IEnumerable<T> Skip<T>(IEnumerable<T> source, int count)
+        {
+            List<T> result = new List<T>();
+            int index = 0;
+            foreach (var item in source)
+            {
+                if (index >= count)
+                {
+                    result.Add(item);
+                }
+                index++;
+            }
+            return result;
+        }
+        public static IEnumerable<T> Take<T>(IEnumerable<T> source, int count)
+        {
+            List<T> result = new List<T>();
+            int index = 0;
+            foreach (var item in source)
+            {
+                if (index < count)
+                {
+                    result.Add(item);
+                }
+                index++;
+            }
+            return result;
+        }
+        public static IEnumerable<T> takeLast<T>(IEnumerable<T> source, int count)
+        {
+            List<T> result = new List<T>();
+            int totalCount = source.Count();
+            int index = 0;
+            foreach (var item in source)
+            {
+                if (index >= totalCount - count)
+                {
+                    result.Add(item);
+                }
+                index++;
+            }
+            return result;
+        }
+        public static IEnumerable<T> skipLast<T>(IEnumerable<T> source, int count)
+        {
+            List<T> result = new List<T>();
+            int totalCount = source.Count();
+            int index = 0;
+            foreach (var item in source)
+            {
+                if (index < totalCount - count)
+                {
+                    result.Add(item);
+                }
+                index++;
+            }
+            return result;
+        }
+        public static IEnumerable<T> TakeWhile<T>(IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            //break is the key difference between where and takeWhile
+            List<T> result = new List<T>();
+            foreach (var item in source)
+            {
+                if (predicate(item))
+                {
+                    result.Add(item);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return result;
+        }
+        public static IEnumerable<T> SkipWhile<T>(IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            List<T> result = new List<T>();
+            bool shouldSkip = true;
+            foreach (var item in source)
+            {
+                if (shouldSkip && predicate(item))
+                {
+                    continue;
+                }
+                else
+                {
+                    shouldSkip = false;
+                    result.Add(item);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ LINQ task 1/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace LINQ_task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var evenNumbers = LINQ_methods.Where(numbers, n => n % 2 == 0);
+            Console.WriteLine("Even numbers:");
+            foreach (var num in evenNumbers)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine("Numbers at even indices:");
+            var evenIndexedNumbers = LINQ_methods.WhereIndex(numbers, (n, index) => index % 2 == 0);
+            foreach (var num in evenIndexedNumbers)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine("Squared numbers:");
+            var squaredNumbers = LINQ_methods.Select(numbers, n => n * n);
+            foreach (var num in squaredNumbers)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine("Numbers with their indices:");
+            var numbersWithIndices = LINQ_methods.SelectIndex(numbers, (n, index) => $"Index {index}: {n}");
+            foreach (var str in numbersWithIndices)
+            {
+                Console.WriteLine(str);
+            }
+            Console.WriteLine("Numbers after skipping first 5:");
+            var skippedNumbers = LINQ_methods.Skip(numbers, 5);
+            foreach (var num in skippedNumbers)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine("Numbers after taking first 3:");
+            var takenNumbers = LINQ_methods.Take(numbers, 3);
+            foreach (var num in takenNumbers)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine("Numbers before number 7");
+            var numbersBeforeSeven = LINQ_methods.TakeWhile(numbers, n => n < 7);
+            foreach (var num in numbersBeforeSeven)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine("Numbers Skipping before 5");
+            var numbersSkippingBeforeFive = LINQ_methods.SkipWhile(numbers, n => n < 5);
+            foreach (var num in numbersSkippingBeforeFive)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
